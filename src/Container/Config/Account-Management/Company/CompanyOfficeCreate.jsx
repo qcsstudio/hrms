a3e64ee0ddfd9a84e41cb2e761ab7d1bdec364f4
pankajqcs <pankajqcss@gmail.com>
2026-02-17 12:17:09 +0530

    add api of incorporation

--- src/Container/Config/Account-Management/Company/CompanyOfficeCreate.jsx
+++ src/Container/Config/Account-Management/Company/CompanyOfficeCreate.jsx
@@ -37,7 +37,7 @@ const CompanyOfficeCreate = () => {
       }
       );
       console.log("Company office created:", res.data);
-      if (res.status === 200) {
+      if (res.status === 201) {
         setFormData({
           locationName: "",
           addressType: "",
