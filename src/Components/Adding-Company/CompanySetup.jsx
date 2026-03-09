aaee7a3bdb009f81ce7951202407b01edee6e254
Viiishal-24 <vishal2411kumar@gmail.com>
2026-03-09 10:20:47 +0530

    changes in Company adding forms UI

--- src/Components/Adding-Company/CompanySetup.jsx
+++ src/Components/Adding-Company/CompanySetup.jsx
@@ -279,13 +279,6 @@ const CompanySetup = ({ onNext, onBack }) => {
               className={`w-full h-10 border border-black/10 rounded-lg pl-3 pr-8 bg-white appearance-none ${
                 formData.industryType ? 'text-[#111827]' : 'text-[#BDBDBD]'
               }`}
-              style={{
-                backgroundImage:
-                  "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='14' height='14' viewBox='0 0 24 24' fill='none' stroke='%23B3B3B3' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpolyline points='6 9 12 15 18 9'%3E%3C/polyline%3E%3C/svg%3E\")",
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'right 10px center',
-                backgroundSize: '14px 14px'
-              }}
             >
               <option value="">Select Industry</option>
               <option value="IT">IT</option>
